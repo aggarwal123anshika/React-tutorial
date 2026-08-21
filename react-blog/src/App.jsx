6809d4f37fd02bc1bcefe8aa1b588b5ee00afa35
anshika <anshikaagarwal9966@gmail.com>
2026-08-21 11:29:05 +0530

    Importing and Exporting Compnents

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,14 +1,34 @@
-import User from './User'
+// // import User from './User'
+// function App() {
+//   return (
+//     <>
+//       <h1>First Component</h1>
+//       <Fruit />
+//       <Color />
+//     </>
+//   )
+// }
+// function Fruit() {
+//   return (
+//     <h1>Apple is a healthy fruit</h1>
+//   )
+// }
+// function Color() {
+//   return (
+//     <h1>Pink is a wonderful color!</h1>
+//   )
+// }
+// export default App;
+
+import Login,{Profile, Setting} from "./UserComponent";
 function App() {
-
-
   return (
     <>
-    <h1>Hello gyes!</h1>
-    <User />
+      <h1>Importing and Exporting Components</h1>
+      <Login />
+      <Profile />
+      <Setting />
     </>
-
   )
 }
-
-export default App
+export default App;
---
+++ react-blog/src/UserComponent.jsx
@@ -0,0 +1,23 @@
+function Login() {
+    return (
+        <>
+            <h1>Login User</h1>
+        </>
+    )
+}
+export default Login;
+export function Profile() {
+    return (
+        <>
+        <h1>Proffile User</h1>
+        </>
+    )
+}
+export function Setting() {
+    return (
+        <>
+        <h1> User Settings</h1>
+        </>
+    )
+}
+
